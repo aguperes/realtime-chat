79ec7e9e1cf860b9b32e4bb48c2db47aec709714
Agustin Peres <agu.peres3@gmail.com>
2025-12-08 15:45:01 -0300

    Setup Home route

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const ANIMALS = [
+  "lion",
+  "tiger",
+  "elephant",
+  "zebra",
+  "giraffe",
+  "monkey",
+  "penguin",
+  "dolphin",
+  "snake",
+  "rabbit",
+];
+
+export const STORAGE_KEY = "chat_username";
