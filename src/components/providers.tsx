a174e3451fe0d89632dc93145ddb80e908972d3d
Agustin Peres <agu.peres3@gmail.com>
2025-12-24 18:07:52 -0300

    Add Messaging Logic

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RealtimeProvider } from "@upstash/realtime/client";
+import { useState } from "react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <RealtimeProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </RealtimeProvider>
   );
 };
